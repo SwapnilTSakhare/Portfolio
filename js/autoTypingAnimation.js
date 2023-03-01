565d83f478c3e5de74d3a07bdd5c123a575b8d8e
SwapnilTSakhare <105933702+SwapnilTSakhare@users.noreply.github.com>
2023-03-01 15:26:29 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,11 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi,<br><br>I am Swapnil Tanaji Sakhare </span><br><br>I am currently pursuing BE in Savitribai Phule Pune University at JSPM'S Bhiwarabai Sawant Institute of Technology and Research Wagholi Pune 412207, from 2021 to onwards.<br><br> I am Hardworking and studious student having knowledge and experience over web development. I am a tech enthusiast seeking and always ready to learn about new technology. Bring forth a motivated attitude and a variety of skills set."
+      ],
+      typeSpeed: 9,
+      contentType: 'html'
+    });
+  }, 100);
+
+  
